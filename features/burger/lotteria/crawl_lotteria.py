98860558961d5885e4cf274e2cc91672190f7a5f
jpaper <bigmooon1215@gmail.com>
2026-02-01 16:30:48 +0900

    refactor: 크롤링 데이터 저장 폴더 구조 변경경

--- features/burger/lotteria/crawl_lotteria.py
+++ features/burger/lotteria/crawl_lotteria.py
@@ -2,12 +2,15 @@ import json
 import html
 
 import requests
+from pathlib import Path
 
 from transform_lotteria import transform_store_data
 
 # 상수
 URL = "https://www.lotteeatz.com/searchStore/getStoresListAjax"
-JSON_FILE = "lotteria.json"
+BASE_DIR = Path(__file__).parent
+DATA_DIR = BASE_DIR / "data"
+JSON_FILE = DATA_DIR / "lotteria.json"
 HEADERS = {
     "Content-Type": "application/json",
     "Accept": "text/html, */*; q=0.01",
@@ -38,22 +41,28 @@ PAYLOAD = {
 
 
 def fetch_store_data():
+    """롯데리아 매장 데이터 받아오기"""
     response = requests.post(URL, json=PAYLOAD, headers=HEADERS)
     response.raise_for_status()
+
     return response.text
 
 
 def extract_store_info(response_text):
+    """html 안에 있는 JSON 데이터 추출"""
     try:
         hidden_input = response_text.split('<input type="hidden" name="storeList_JSON" value="')[1]
         raw_json = hidden_input.split('" />')[0]
         decoded_json = html.unescape(raw_json)
+
         return json.loads(decoded_json)
+
     except (IndexError, json.JSONDecodeError) as e:
         raise Exception('[Lotteria]: Failed to extract or decode JSON') from e
 
 
 def save_to_json(data, filepath):
+    filepath.parent.mkdir(parents=True, exist_ok=True)
     with open(filepath, mode='w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
